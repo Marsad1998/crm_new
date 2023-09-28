ecca9b16d5e2fedf5263d6f5646ac049a89be42b
dennis-denhollander <wordpresswebdesignseo@gmail.com>
2023-09-29 02:12:40 +0500

    fix the migrations

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -30,15 +30,7 @@ class DatabaseSeeder extends Seeder
         ]);
 
         $tenant->domains()->create([
-            'domain' => 'test.localhost',
-        ]);
-
-        $tenant = Tenant::create([
-            'id' => 'quotegen1',
-        ]);
-
-        $tenant->domains()->create([
-            'domain' => 'test1.localhost',
+            'domain' => 'test.montyslocksmith.ca',
         ]);
     }
 }
